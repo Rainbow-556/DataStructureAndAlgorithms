6abefb6cf9a65fce03e92289a527c1016397a7fa
lixiang <903263494@qq.com>
2018-07-01 21:40:13 +0800

    1.完善快速排序算法

--- app/src/main/java/com/datastructureandalgorithms/app/SortPracticeActivity.java
+++ app/src/main/java/com/datastructureandalgorithms/app/SortPracticeActivity.java
@@ -38,40 +38,46 @@ public final class SortPracticeActivity extends AppCompatActivity implements Vie
      * j从begin开始从左往右扫描，如果遇到比基数大的直接与i进行交换，然后执行i的从右到左扫描
      * 以上两个步骤循环，直到i和j相遇时，此时i和j的位置标识为k，则k左边的数都比k小，k右边的数都比k大，然后对左右两部分的数据分别进行快排，
      * 如果发现某一个部分只有一个元素时，则不需要排，否则对这部分的数进行快排
+     *
      * @param arr
      * @param begin
      * @param end
      */
     private void quickSort(int[] arr, int begin, int end) {
+        // 当某个部分只剩下一个或者没有元素时，则该部分不需要排序
+        if (begin >= end) {
+            return;
+        }
+        int i = begin, j = end;
+        // 基数
         int base = arr[begin];
-        int i = end, j = begin;
-        int k;
-        boolean lookI = true;
-        while (j < i) {
-            if (lookI) {
-                if (arr[i] < base) {
-                    swap(arr, i, j);
-                } else {
-                    i--;
-                    continue;
-                }
+        // 知道i和j相遇才跳出循环
+        while (i < j) {
+            // 从j处开始从右往左扫描，直到某个位置的元素比base小，则把该位置上的元素赋值给角标为i的元素
+            while (i < j && arr[j] > base) {
+                j--;
             }
-            if (arr[j] > base) {
-                swap(arr, j, i);
-                lookI = true;
-            } else {
-                j++;
-                lookI = false;
-                continue;
+            if (i < j) {
+                arr[i] = arr[j];
+                // 赋值之后i向右移一位
+                i++;
+            }
+            // 此时从i处开始从左往右扫描，直到某个位置上的元素比base大，则把该位置上的元素赋值给角标为j的元素
+            while (i < j && arr[i] < base) {
+                i++;
+            }
+            if (i < j) {
+                arr[j] = arr[i];
+                // 赋值之后j往左移一位
+                j--;
             }
         }
-        k = i;
-        if (k - begin > 1) {
-            quickSort(arr, begin, k - 1);
-        }
-        if (end - k > 1) {
-            quickSort(arr, k + 1, end);
-        }
+        // 跳出循环后，则代表i和j相遇了，此时直接把角标为i的元素赋值为base
+        arr[i] = base;
+        // 执行到这里，表示角标i左边的元素都比base小，而角标i右边的元素都比i大，从而把一个无序数组分离成了两个部分
+        // 然后对这两个部分分别再进行快速排序
+        quickSort(arr, begin, i - 1);
+        quickSort(arr, i + 1, end);
     }
 
     private void swap(int[] arr, int m, int n) {
